93d99d404bab8f0764e515b3045b50ca8e6fe7e9
csalvite <csalvite@gmail.com>
2023-07-15 18:04:21 +0200

    header v1

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>
+        Adriana Lago <span>Photographer</span>
+      </h1>
+      <ul>
+        <li>Narrative</li>
+        <li>Music Videos</li>
+        <li>Comercials</li>
+        <li>Info</li>
+        <li>Stills</li>
+      </ul>
+    </header>
+  );
+};
