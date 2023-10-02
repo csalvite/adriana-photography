ed46b60ba47cb9e265a8035c766f8a47fab33df6
csalvite <csalvite@gmail.com>
2023-10-02 15:26:25 +0200

    page photos ok

--- src/pages/Photos/Photos.js
+++ src/pages/Photos/Photos.js
@@ -3,8 +3,6 @@ import { useEffect, useState } from 'react';
 import Footer from '../../components/Footer/Footer';
 import { Header } from '../../components/Header/Header';
 import 'react-image-gallery/styles/css/image-gallery.css';
-import ImageCarousel from '../../components/PhotoCarrousel/ImageCarrousel';
-import Jobs from '../../components/Jobs/Jobs';
 import WSPGallery from '../../components/PhotoCarrousel/WSPGallery';
 import './Photos.css';
 
@@ -18,8 +16,9 @@ const Photos = () => {
 
   useEffect(() => {
     const newImages = [];
+
     const photoFiles = importAll(
-      require.context('../../assets/trabajo1', false, /\.(png|jpe?g|svg)$/)
+      require.context('../../assets', true, /\.(jpg|jpeg|png|gif|bmp)$/)
     );
 
     photoFiles.map((photo) =>
@@ -38,16 +37,6 @@ const Photos = () => {
       <div className='hr'>
         <hr />
       </div>
-      {/* <h2>Trabajo de moda</h2> */}
-      <h3>Trabajo de moda</h3>
-      <p>Con fecha de 02/10/2022</p>
-      <ImageCarousel images={images} />
-      <br />
-      <br />
-      <h2>Gustache mais o de arriba ou o de abaixo?¿</h2>
-      <br />
-      <br />
-
       <WSPGallery images={images} />
       <Footer />
     </div>
