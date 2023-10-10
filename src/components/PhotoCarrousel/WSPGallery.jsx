6a55420d34a4ad99a708987c1596987bdc75979c
csalvite <csalvite@gmail.com>
2023-10-10 11:05:09 +0200

    added key events to slide images on openModal

--- src/components/PhotoCarrousel/WSPGallery.jsx
+++ src/components/PhotoCarrousel/WSPGallery.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './PhotosCarrousel.css';
 
 const { REACT_APP_BACK } = process.env;
 
+const useKey = (key, callback) => {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  useEffect(() => {
+    function handle(e) {
+      if (e.code === key) {
+        callbackRef.current(e);
+      }
+    }
+
+    document.addEventListener('keydown', handle);
+    return () => document.removeEventListener('keypress', handle);
+  }, [key]);
+};
+
 const WSPGallery = ({ images, sectionTitle }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
+
+  function handleKeySlideImageNext() {
+    if (openModal) {
+      nextSlide();
+    }
+  }
+
+  function handleKeySlideImagePrev() {
+    if (openModal) {
+      prevSlide();
+    }
+  }
 
   const handleOpenModal = (index) => {
     setSlideNumber(index);
@@ -27,6 +58,11 @@ const WSPGallery = ({ images, sectionTitle }) => {
       ? setSlideNumber(0)
       : setSlideNumber(slideNumber + 1);
   };
+
+  // Keys refs
+  useKey('ArrowRight', handleKeySlideImageNext);
+  useKey('ArrowLeft', handleKeySlideImagePrev);
+  useKey('Escape', handleCloseModal);
 
   return (
     <div className='wspGallery'>
