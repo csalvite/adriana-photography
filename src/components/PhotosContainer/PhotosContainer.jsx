322b3b0ee272d58ea9591a35bb0482da3f0819ed
César Alvite <csalvite@gmail.com>
2023-10-18 22:50:58 +0200

    probando nuevo estilos para photos

---
+++ src/components/PhotosContainer/PhotosContainer.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import Reveal from '../Reveal';
+
+const { REACT_APP_BACK } = process.env;
+
+const PhotosContainer = ({ idImagen, title, image, onClick }) => {
+  const [photosCover, setPhotosCover] = useState();
+
+  return (
+    <div className='container'>
+      <div className='img-reveal'>
+        {photosCover &&
+          photosCover?.map((img, index) => {
+            return (
+              <Reveal delay={0.25 + index / 10}>
+                <img
+                  key={index}
+                  src={`${REACT_APP_BACK}/${title.title}/${img.name}`}
+                  alt={`title${idImagen}`}
+                />
+              </Reveal>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default PhotosContainer;
