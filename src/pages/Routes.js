76cae7952259a16c169daf356a954b553e60a4a1
csalvite <csalvite@gmail.com>
2023-10-31 13:58:16 +0100

    creamos pagina de login para subir nuevo contenido

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -3,6 +3,7 @@ import Home from './Home/Home';
 import Info from './Info/Info';
 import Photos from './Photos/Photos';
 import Videos from './Videos/Videos';
+import Login from './Login/Login';
 
 function MyRoutes() {
   return (
@@ -13,6 +14,8 @@ function MyRoutes() {
         <Route path='/info' element={<Info />} />
         <Route path='/photos' element={<Photos />} />
         <Route path='/videos' element={<Videos />} />
+        {/* Pagina para login */}
+        <Route path='/acceso' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
