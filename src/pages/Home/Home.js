30e3c4694935e0d42d28c9c81a27a008f6261880
csalvite <csalvite@gmail.com>
2023-11-07 10:08:47 +0100

    quito componentes que no uso

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,9 @@
-import { useContext } from 'react';
 import { Header } from '../../components/Header/Header';
 import Job from '../../components/Jobs/Job';
 import './Home.css';
-import { PhotosContext } from '../../context/PhotosContext';
 import { Logout } from '../../components/Logout';
 
 function Home() {
-  const { images, error, loading } = useContext(PhotosContext);
-
   return (
     <div className='App'>
       <Header />
